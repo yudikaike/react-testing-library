94ac643c9921a3ce12c861ea05a3fc60a2c7e0ef
Kaike Yudi <kaikeyudi@outlook.com>
2022-03-22 17:16:57 -0300

    req-3: implementa teste da exibição da mensagem 'No favorite pokemon found' do componente 'FavoritePokemons'

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testa o componente FavoritePokemons', () => {
+  test('Exibe na tela a mensagem "No favorite pokemon found"', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const favoriteLink = screen.getByRole('link', {
+      name: 'Favorite Pokémons',
+    });
+
+    userEvent.click(favoriteLink);
+
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/favorites');
+
+    const noFavoritePokemons = screen.getByText('No favorite pokemon found');
+    expect(noFavoritePokemons).toBeInTheDocument();
+  });
+});
