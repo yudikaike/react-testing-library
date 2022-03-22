6f70586ea2e54afda54983d23254e63b2f109a78
Kaike Yudi <kaikeyudi@outlook.com>
2022-03-22 15:49:13 -0300

    req-1: implementa o teste do link 'Home' no componente 'App'

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testa o componente App', () => {
+  test('Verifica se o link possui o texto "Home" e leva ao endereço "/"', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const homeLink = screen.getByRole('link', {
+      name: 'Home',
+    });
+    expect(homeLink).toBeInTheDocument();
+
+    userEvent.click(homeLink);
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/');
+  });
+});
