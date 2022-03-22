516b8e56c334403f9e29074539b11edd85c4241d
Kaike Yudi <kaikeyudi@outlook.com>
2022-03-22 17:35:27 -0300

    req-4: implementa teste do heading do componente 'NotFound'

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testa o componente NotFound', () => {
+  test('Contém um heading com o texto "Page requested not found 😭"', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFoundText = screen.getByRole('heading', {
+      level: 2,
+      name: 'Page requested not found Crying emoji',
+    });
+
+    expect(notFoundText).toBeInTheDocument();
+  });
+});
