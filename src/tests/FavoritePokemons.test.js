0966f9553ffebc951dc7d240a408b5254b77122e
Kaike Yudi <kaikeyudi@outlook.com>
2022-03-22 17:18:37 -0300

    req-3: implementa teste da exibição dos pokémons favoritos do componente 'FavoritePokemons'

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -20,4 +20,30 @@ describe('Testa o componente FavoritePokemons', () => {
     const noFavoritePokemons = screen.getByText('No favorite pokemon found');
     expect(noFavoritePokemons).toBeInTheDocument();
   });
+
+  test('Exibe os pokémons favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const detailsLink = screen.getByRole('link', {
+      name: 'More details',
+    });
+
+    userEvent.click(detailsLink);
+
+    const favoriteCheckbox = screen.getByRole('checkbox');
+
+    userEvent.click(favoriteCheckbox);
+
+    const favoritesLink = screen.getByRole('link', {
+      name: 'Favorite Pokémons',
+    });
+
+    userEvent.click(favoritesLink);
+
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/favorites');
+
+    const favoritePokemons = screen.getByText('Pikachu');
+    expect(favoritePokemons).toBeInTheDocument();
+  });
 });
