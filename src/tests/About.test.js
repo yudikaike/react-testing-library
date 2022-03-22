540ab44ab29f052408dcb6cb20a38efaf152109b
Kaike Yudi <kaikeyudi@outlook.com>
2022-03-22 16:13:14 -0300

    req-2: implementa teste do header do componente 'About'

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+describe('Testa o componente About', () => {
+  test('Contém um heading com o texto "About Pokédex"', () => {
+    renderWithRouter(<About />);
+
+    const header = screen.getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    });
+
+    expect(header).toBeInTheDocument();
+  });
+});
