4b86caf307ee28109f51db7bd6d80f506e2c0923
Kaike Yudi <kaikeyudi@outlook.com>
2022-03-22 16:37:37 -0300

    req-2: implementa teste da imagem do componente 'About'

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -24,4 +24,12 @@ describe('Testa o componente About', () => {
     expect(paragraph1).toBeInTheDocument();
     expect(paragraph2).toBeInTheDocument();
   });
+
+  test('Contém uma imagem da pokédex"', () => {
+    renderWithRouter(<About />);
+
+    const { src } = screen.getByRole('img');
+
+    expect(src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
 });
