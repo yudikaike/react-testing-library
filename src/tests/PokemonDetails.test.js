4cfc6c2f3cfa75ef69fdd66b85c21a8af61ea2f8
Kaike Yudi <kaikeyudi@outlook.com>
2022-03-23 14:06:24 -0300

    req-7: implementa teste das informações do componente 'PokemonDetails'

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testa componente PokemonDetails', () => {
+  const linkName = 'More details';
+  test('Contém informações do pokémon selecionado', () => {
+    renderWithRouter(<App />);
+
+    const detailsLink = screen.getByRole('link', {
+      name: linkName,
+    });
+    userEvent.click(detailsLink);
+
+    const detailsText = screen.getByText('Pikachu Details');
+    expect(detailsText).toBeInTheDocument();
+    expect(detailsLink).not.toBeInTheDocument();
+
+    const summary = screen.getByRole('heading', {
+      level: 2,
+      name: 'Summary',
+    });
+    expect(summary).toBeInTheDocument();
+
+    const description = screen.getByText(/This intelligent/i);
+    expect(description).toBeInTheDocument();
+  });
+});
