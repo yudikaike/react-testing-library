694052f0a731c62d2af5f37d5f3e724249b5dab3
Kaike Yudi <kaikeyudi@outlook.com>
2022-03-23 12:50:26 -0300

    req-5: implementa teste do heading do componente 'Pokedex'

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testa o componente Pokedex', () => {
+  test('Contém um heading com o texto "Encountered pokémons"', () => {
+    renderWithRouter(<App />);
+
+    const heading = screen.getByText('Encountered pokémons');
+
+    expect(heading).toBeInTheDocument();
+  });
+});
