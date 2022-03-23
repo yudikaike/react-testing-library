4020ea1122ec68fc91bc289976068658cb92df35
Kaike Yudi <kaikeyudi@outlook.com>
2022-03-23 12:52:04 -0300

    req-5: implementa teste do botão 'Próximo pokémon' do componente 'Pokedex'

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
@@ -10,5 +11,29 @@ describe('Testa o componente Pokedex', () => {
     const heading = screen.getByText('Encountered pokémons');
 
     expect(heading).toBeInTheDocument();
+  });
+
+  test('É exibido o próximo pokémon quando se é clicado no botão', () => {
+    renderWithRouter(<App />);
+
+    const pikachu = screen.getByText('Pikachu');
+    const nextPokemonBtn = screen.getByRole('button', {
+      name: 'Próximo pokémon',
+    });
+
+    expect(pikachu).toBeInTheDocument();
+    expect(nextPokemonBtn).toBeInTheDocument();
+
+    userEvent.click(nextPokemonBtn);
+
+    const charmander = screen.getByText('Charmander');
+    expect(charmander).toBeInTheDocument();
+
+    const clickTimes = 8;
+    for (let index = 0; index < clickTimes; index += 1) {
+      userEvent.click(nextPokemonBtn);
+    }
+
+    expect(screen.getByText('Pikachu')).toBeInTheDocument();
   });
 });
