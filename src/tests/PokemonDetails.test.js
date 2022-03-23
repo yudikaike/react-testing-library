66b4dee4749e2cb8df3031eb3385f0dfde70ebf4
Kaike Yudi <kaikeyudi@outlook.com>
2022-03-23 14:06:56 -0300

    req-7: implementa teste das localidades do componente 'PokemonDetails'

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -27,4 +27,29 @@ describe('Testa componente PokemonDetails', () => {
     const description = screen.getByText(/This intelligent/i);
     expect(description).toBeInTheDocument();
   });
+
+  test('Contém uma seção com mapas', () => {
+    renderWithRouter(<App />);
+
+    const detailsLink = screen.getByRole('link', {
+      name: linkName,
+    });
+    userEvent.click(detailsLink);
+
+    const locationsText = screen.getByText('Game Locations of Pikachu');
+    expect(locationsText).toBeInTheDocument();
+
+    const locations = screen.getAllByAltText('Pikachu location');
+    expect(locations).toHaveLength(2);
+
+    const { src: src1 } = locations[0];
+    expect(src1).toBe('https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+    const location1Text = screen.getByText('Kanto Viridian Forest');
+    expect(location1Text).toBeInTheDocument();
+
+    const { src: src2 } = locations[1];
+    expect(src2).toBe('https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png');
+    const location2Text = screen.getByText('Kanto Power Plant');
+    expect(location2Text).toBeInTheDocument();
+  });
 });
